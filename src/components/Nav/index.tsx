fc5ee4c009451c5f346e7cf6bbf0f856a69aa04c
houseplayer <lukasz.szymczyk@gmail.com>
2024-11-23 15:55:47 +0100

    nav rendered conditionally depending on user login state

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,19 +1,27 @@
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 const Nav = () => {
+  const { user } = useUser();
+
   return (
     <>
-      <div>
-        <a href="/api/auth/login" className="mx-2" tabIndex={0}>
-          Log in
-        </a>
-
+      {user ? (
         <a href="/api/auth/logout" className="mx-2" tabIndex={0}>
           Log out
         </a>
+      ) : (
+        <div>
+          <a href="/api/auth/login" className="mx-2" tabIndex={0}>
+            Log in
+          </a>
 
-        <a href="/api/auth/signup" className="mx-2" tabIndex={0}>
-          Sign Up
-        </a>
-      </div>
+          <a href="/api/auth/signup" className="mx-2" tabIndex={0}>
+            Sign Up
+          </a>
+        </div>
+      )}
     </>
   );
 };
