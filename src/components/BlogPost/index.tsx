4fff8f8dc29dc474f48815ae03b6969a1e1cec2d
houseplayer <lukasz.szymczyk@gmail.com>
2024-12-02 15:18:04 +0100

    fix public directory error

--- src/components/BlogPost/index.tsx
+++ src/components/BlogPost/index.tsx
@@ -7,9 +7,12 @@ interface Props {
 }
 
 const BlogPost = async ({ blogId }: Props) => {
-  // const markdown = fs.readFileSync(`src/public/${routes.blog}/${blogId}.md`, 'utf-8');
-  // return <MarkdownToHtml markdown={markdown} />;
-  return <p>{blogId}</p>;
+  const markdown = fs.readFileSync(
+    `${process.cwd()}/src/public/${routes.blog}/${blogId}.md`,
+    'utf-8',
+  );
+  return <MarkdownToHtml markdown={markdown} />;
+  // return <p>{blogId}</p>;
 };
 
 export default BlogPost;
