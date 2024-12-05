6d4c1252642e45ed05073f1e1cd8df99ec4ff023
houseplayer <lukasz.szymczyk@gmail.com>
2024-12-06 00:47:22 +0100

    remove unused folders

--- src/components/emailForm/schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const sendEmailSchema = z.object({
-  title: z.string().min(1, 'enter email title'),
-  content: z.string().min(1, 'enter email content'),
-});
-export type SendEmailSchema = z.infer<typeof sendEmailSchema>;
