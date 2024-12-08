6e35525c42dc0aa083db468513e65a3ca4a02160
houseplayer <lukasz.szymczyk@gmail.com>
2024-12-08 14:03:06 +0100

    add test cases for posts

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
+
+module.exports = createJestConfig(customJestConfig);
