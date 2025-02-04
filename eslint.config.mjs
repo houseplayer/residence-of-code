459c6d10ae3ad74eb3fa216cf6ebcbb6136519a3
houseplayer <lukasz.szymczyk@gmail.com>
2025-02-04 09:09:00 +0100

    add prettier config in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,18 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
 import pluginReact from "eslint-plugin-react"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	{
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 	pluginReact.configs.flat.recommended,
+	eslintPluginPrettierRecommended,
 	{
 		rules: {
 			"no-console": "error",
@@ -23,6 +27,6 @@ export default [
 		},
 	},
 	{
-		ignores: [".next/**/*", "node_modules/**/*"],
+		ignores: [".next/**/*", "node_modules/**/*", "coverage/**/*"],
 	},
 ]
